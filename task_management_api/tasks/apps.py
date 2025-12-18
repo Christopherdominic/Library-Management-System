6439d3e3c2e6bf72ded28ae415228be0364236e3
you@christopherdominic <chriseze0@gmail.com>
2025-12-18 15:58:53 +0100

    added to apps

--- task_management_api/tasks/apps.py
+++ task_management_api/tasks/apps.py
@@ -1,5 +1,22 @@
 from django.apps import AppConfig
-
+import os
 
 class TasksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if username and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password
+                )
+
